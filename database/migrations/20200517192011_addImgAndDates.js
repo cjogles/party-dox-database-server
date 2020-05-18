ef5427b917b3a0e000a3fb8df2f11cc00446dd43
cjogles <jackson.ogles28@gmail.com>
2020-05-17 20:18:54 -0600

    edits to migration file

--- database/migrations/20200517192011_addImgAndDates.js
+++ database/migrations/20200517192011_addImgAndDates.js
@@ -1,54 +1,39 @@
 exports.up = function (knex) {
   return knex.schema
-    .alterTable("friends", (friend) => {
+    .table("friends", (friend) => {
       friend.binary("friend_profile_pic").alter();
     })
-    .alterTable("trips", (trip) => {
-      trip
-        .timestamp("trip_start_date", { precision: 6 })
-        .defaultTo(knex.fn.now(6))
-        .alter();
-      trip
-        .timestamp("trip_end_date", { precision: 6 })
-        .defaultTo(knex.fn.now(6))
-        .alter();
+    .table("trips", (trip) => {
+      trip.timestamp("trip_start_date").defaultTo(knex.fn.now(6)).alter();
+      trip.timestamp("trip_end_date").defaultTo(knex.fn.now(6)).alter();
     })
-    .alterTable("activities", (activity) => {
+    .table("activities", (activity) => {
       activity
-        .timestamp("activity_start_date", { precision: 6 })
+        .timestamp("activity_start_date")
         .defaultTo(knex.fn.now(6))
         .alter();
-      activity
-        .timestamp("activity_end_date", { precision: 6 })
-        .defaultTo(knex.fn.now(6))
-        .alter();
+      activity.timestamp("activity_end_date").defaultTo(knex.fn.now(6)).alter();
     })
-    .alterTable("flights", (flight) => {
-      flight
-        .timestamp("departure_date", { precision: 6 })
-        .defaultTo(knex.fn.now(6))
-        .alter();
-      flight
-        .timestamp("arrival_date", { precision: 6 })
-        .defaultTo(knex.fn.now(6))
-        .alter();
+    .table("flights", (flight) => {
+      flight.timestamp("departure_date").defaultTo(knex.fn.now(6)).alter();
+      flight.timestamp("arrival_date").defaultTo(knex.fn.now(6)).alter();
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
-    .alterTable("friends", (friend) => {
+    .table("friends", (friend) => {
       friend.string("friend_profile_pic").alter();
     })
-    .alterTable("trips", (trip) => {
+    .table("trips", (trip) => {
       trip.string("trip_start_date").alter();
       trip.string("trip_end_date").alter();
     })
-    .alterTable("activities", (activity) => {
+    .table("activities", (activity) => {
       activity.string("activity_start_date").alter();
       activity.string("activity_end_date").alter();
     })
-    .alterTable("flights", (flight) => {
+    .table("flights", (flight) => {
       flight.string("departure_date").alter();
       flight.string("arrival_date").alter();
     });
