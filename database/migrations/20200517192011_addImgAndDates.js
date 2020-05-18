8850885f4b65c97175ad9fd34de2d0d78859683e
cjogles <jackson.ogles28@gmail.com>
2020-05-17 20:05:33 -0600

    added alter to image and date migration

--- database/migrations/20200517192011_addImgAndDates.js
+++ database/migrations/20200517192011_addImgAndDates.js
@@ -1,37 +1,37 @@
 exports.up = function (knex) {
   return knex.schema
     .alterTable("friends", (friend) => {
-      friend.binary("friend_profile_pic");
+      friend.binary("friend_profile_pic").alter();
     })
     .alterTable("trips", (trip) => {
-      trip.timestamp("trip_start_date");
-      trip.timestamp("trip_end_date");
+      trip.timestamp("trip_start_date").alter();
+      trip.timestamp("trip_end_date").alter();
     })
     .alterTable("activities", (activity) => {
-      activity.timestamp("activity_start_date");
-      activity.timestamp("activity_end_date");
+      activity.timestamp("activity_start_date").alter();
+      activity.timestamp("activity_end_date").alter();
     })
     .alterTable("flights", (flight) => {
-      flight.timestamp("departure_date");
-      flight.timestamp("arrival_date");
+      flight.timestamp("departure_date").alter();
+      flight.timestamp("arrival_date").alter();
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
     .alterTable("friends", (friend) => {
-      friend.string("friend_profile_pic");
+      friend.string("friend_profile_pic").alter();
     })
     .alterTable("trips", (trip) => {
-      trip.string("trip_start_date");
-      trip.string("trip_end_date");
+      trip.string("trip_start_date").alter();
+      trip.string("trip_end_date").alter();
     })
     .alterTable("activities", (activity) => {
-      activity.string("activity_start_date");
-      activity.string("activity_end_date");
+      activity.string("activity_start_date").alter();
+      activity.string("activity_end_date").alter();
     })
     .alterTable("flights", (flight) => {
-      flight.string("departure_date");
-      flight.string("arrival_date");
+      flight.string("departure_date").alter();
+      flight.string("arrival_date").alter();
     });
 };
